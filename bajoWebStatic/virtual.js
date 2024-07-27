5be794c806d8f37329b9bf4c988e35c0589a643b
Ardhi Lukianto <ardhi@lukianto.com>
2024-07-27 11:02:42 +0700

    Mount virtual & how to inject script in theme

--- bajoWebStatic/virtual.js
+++ bajoWebStatic/virtual.js
@@ -1,12 +1,11 @@
 async function virtual () {
+  const { readConfig } = this.app.bajo
+  const { pick } = this.app.bajo.lib._
+  const libs = await readConfig('bajoWebMpaLibs:/lib/libs.json')
   const virts = []
-  if (this.config.virtuals.jquery) virts.push({ prefix: 'jquery', root: 'jquery:/dist' })
-  if (this.config.virtuals.prismjs) virts.push({ root: 'prismjs:', prefix: 'prismjs' })
-  if (this.config.virtuals.masonry) virts.push({ prefix: 'masonry', root: 'masonry-layout:/dist' })
-  if (this.config.virtuals.highlightjs) virts.push({ prefix: 'highlight.js', root: '@highlightjs/cdn-assets:' })
-  if (this.config.virtuals.imagesloaded) virts.push({ prefix: 'imagesloaded', root: 'imagesloaded:' })
-  if (this.config.virtuals.tempusDominus) virts.push({ prefix: 'tempus-dominus', root: '@eonasdan/tempus-dominus:/dist' })
-  if (this.config.virtuals.echarts) virts.push({ prefix: 'echarts', root: 'echarts:/dist' })
+  for (const l of libs) {
+    if (this.config.virtuals.includes(l.prefix)) virts.push(pick(l, ['prefix', 'root']))
+  }
   return virts
 }
 
